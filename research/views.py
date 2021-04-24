40b0bdb8c041852781ef6fb2060ba8b2a16ac4d9
parttimehacker@gmail.com <parttimehacker@gmail.com>
2021-04-24 12:26:43 -0700

    Commit of html stuff

--- research/views.py
+++ research/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
-from .models import Content
-from .models import PlatformType
+from .models import Volume, Content, Frequency
 
 
 def dashboard_view(request):
-    static_list = Content.objects.filter(platform=PlatformType.RASPBERRYPI)
-    dynamic_list = Project.objects.filter(platform=PlatformType.ESPRESSIF)
-    arduino_list = Project.objects.filter(platform=PlatformType.ARDUINO)
-    context = {'pi_list': pi_list, 'esp_list': esp_list, 'arduino_list': arduino_list, 'title': 'Internet of Things'}
-    return render(request, 'iot/iot.html', context)
+    volume_list = Volume.objects.all()
+    content_list = Content.objects.all()
+    context = {'volume_list': volume_list, 'content_list': content_list,'title': 'Dashboard'}
+    return render(request, 'research/dashboard.html', context)
+
+def newsletter_view(request):
+    static_list = Content.objects.filter(frequency=Frequency.STATIC)
+    dynamic_list = Content.objects.filter(frequency=Frequency.DYNAMIC)
+    context = {'static_list': static_list, 'dynamic_list': dynamic_list,'title': 'Newsletter'}
+    return render(request, 'research/newsletter.html', context)
